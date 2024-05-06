7f20815342b3cb1d1ae629488f719801f6856e29
Dag <dagmara.szadkowska@wp.pl>
2024-05-06 17:02:24 +0200

    feat: add title component

---
+++ src/assets/components/atoms/MainTitle/MainTitle.jsx
@@ -0,0 +1,11 @@
+import styles from './MainTitle.module.scss'
+
+const MainTitle = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Stabbed</h1>
+    </div>
+  )
+}
+
+export { MainTitle }
