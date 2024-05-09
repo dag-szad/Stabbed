95441c010f487e0200355a1deb954efdc04ba25f
Dag <dagmara.szadkowska@wp.pl>
2024-05-09 18:39:38 +0200

    feat: change logo html structure

--- src/assets/components/atoms/Logo/Logo.jsx
+++ src/assets/components/atoms/Logo/Logo.jsx
@@ -4,11 +4,7 @@ import styles from './Logo.module.scss'
 const Logo = ({ colorVariant }) => {
   const classes = `${styles.logo} ${styles[colorVariant]}`
 
-  return (
-    <div>
-      <img src={iconLogo} className={classes} alt="Logo" />
-    </div>
-  )
+  return <img src={iconLogo} className={classes} alt="Logo" />
 }
 
 export { Logo }
