962a0c0625d5c3dd7a79b8a1c10ed312c8d5d016
Dag <dagmara.szadkowska@wp.pl>
2024-05-14 20:33:22 +0200

    feat: add flipping button

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 
 import { Bases } from './assets/visualizer/bases/Bases.jsx'
 import { Earring } from './assets/visualizer/earrings/Earring.jsx'
 import { BaseSelector } from './assets/visualizer/bases/BaseSelector.jsx'
 import { EarringSelector } from './assets/visualizer/earrings/EarringSelector.jsx'
+import { FlipButton } from './assets/visualizer/bases/FlipButton.jsx'
 import './App.scss'
 
 function App() {
-  const [selectedBase, setSelectedBase] = useState('ear');
-  const [selectedEarrings, setSelectedEarrings] = useState([]);
+  const [base, setBase] = useState({ type: 'ear' })
+  const [selectedEarrings, setSelectedEarrings] = useState([])
+  const [isRotated, setIsRotated] = useState(false)
+  const [containerDimensions, setContainerDimensions] = useState({
+    width: 0,
+    height: 0,
+  })
 
   const changeBase = (newBase) => {
-    setSelectedBase({ ...newBase, fileWidth: newBase.width, fileHeight: newBase.height });
-  };
+    setBase(newBase)
+  }
 
   const addEarring = (earring) => {
     setSelectedEarrings([...selectedEarrings, earring])
   }
 
-  return (
-    <div>
-      <BaseSelector changeBase={changeBase} />
-      <Bases id="dropspace" selectedBase={selectedBase} />
+  const handleImgFlip = () => {
+    setIsRotated(!isRotated)
+  }
 
-      <EarringSelector addEarring={addEarring}/>
+  return (
+<div>
+      <BaseSelector changeBase={changeBase} />
+      <Bases
+        selectedBase={base}
+        isRotated={isRotated}
+        setContainerDimensions={setContainerDimensions}
+      />
+      <EarringSelector addEarring={addEarring} />
       {selectedEarrings.map((earring, index) => (
-        <Earring key={index} earring={earring} />
+        <Earring
+          key={index}
+          earring={earring}
+          isRotated={isRotated}
+          containerDimensions={containerDimensions}
+        />
       ))}
+      <FlipButton onClick={handleImgFlip} />
     </div>
   )
 }
