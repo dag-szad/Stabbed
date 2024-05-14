2a04adfebee7409d5982ae5ac2b495c592e62552
Dag <dagmara.szadkowska@wp.pl>
2024-05-14 20:32:58 +0200

    fix: change base selector logic

--- src/assets/visualizer/bases/BaseSelector.jsx
+++ src/assets/visualizer/bases/BaseSelector.jsx
@@ -1,46 +1,36 @@
-import React, { useState } from 'react';
-
 const BaseSelector = ({ changeBase }) => {
-  const [fileWidth, setFileWidth] = useState(0);
-  const [fileHeight, setFileHeight] = useState(0);
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0]
+    const reader = new FileReader()
 
-  const handleFileUpload = (newFile) => {
-    const file = newFile.target.files[0];
-    const reader = new FileReader();
-  
     reader.onload = (e) => {
-      const img = new Image();
-      img.src = e.target.result;
+      const img = new Image()
+      img.src = e.target.result
       img.onload = () => {
-        if (img.width > 0 && img.height > 0) {
-          setFileHeight(img.height);
-          setFileWidth(img.width);
-          changeBase({
-            url: e.target.result,
-            file: file,
-          });
-        } else {
-          console.error('Invalid image dimensions');
-        }
-      };
-    };
-    reader.readAsDataURL(file);
-  };
+        changeBase({
+          type: 'custom',
+          url: e.target.result,
+          file: file,
+        })
+      }
+    }
+    reader.readAsDataURL(file)
+  }
 
   return (
     <div>
-      <button type="button" onClick={() => changeBase('ear')}>
+      <button type="button" onClick={() => changeBase({ type: 'ear' })}>
         Ear
       </button>
-      <button type="button" onClick={() => changeBase('lips')}>
+      <button type="button" onClick={() => changeBase({ type: 'lips' })}>
         Lips
       </button>
-      <button type="button" onClick={() => changeBase('nose')}>
+      <button type="button" onClick={() => changeBase({ type: 'nose' })}>
         Nose
       </button>
       <input type="file" onChange={handleFileUpload} />
     </div>
-  );
-};
+  )
+}
 
-export { BaseSelector };
+export { BaseSelector }
